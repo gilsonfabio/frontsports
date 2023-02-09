3f298df494260e98ad83564d03dfa47a55842a33
Gilson Fábio <gilsonfabio@gmail.com>
2023-02-09 15:18:16 -0300

    Alteração da chamada da api backend

--- src/pages/api/api.tsx
+++ src/pages/api/api.tsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const api = axios.create({
   //baseURL: "https://transparencia.aparecida.go.gov.br/portal-transparencia/assets/json/faq.json",
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL
+  //baseURL: process.env.NEXT_PUBLIC_BASE_URL
+  baseURL: 'https://git.heroku.com/backsports.git'
 });
 
 export default api;
