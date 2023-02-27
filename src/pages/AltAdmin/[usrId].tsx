1b64e89e103ac7f0dd45f0ed14730c7e7af4dd32
Gilson Fábio <gilsonfabio@gmail.com>
2023-02-27 12:30:19 -0300

    Correção do formato data

--- src/pages/AltAdmin/[usrId].tsx
+++ src/pages/AltAdmin/[usrId].tsx
@@ -57,8 +57,8 @@ const AltAdmin = () => {
         setIdUsuario(usrId);
         console.log('data antes',nasc)
         let datNasc = nasc.substring(6,10) + '-' + nasc.substring(3,5) + '-' + nasc.substring(0,2);
-        setNascimento('data após', datNasc); 
-        console.log(usrNascimento)
+        setNascimento(datNasc); 
+        console.log('data após',usrNascimento)
         try {
             api.put(`updUsuario/${idUsr}`, {
                 usrNome, 
