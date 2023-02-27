02c675a38adfcc7293d7e7ad7f4941031c074a52
Gilson Fábio <gilsonfabio@gmail.com>
2023-02-27 11:23:06 -0300

    Alteração formato data

--- src/pages/AltAdmin/[usrId].tsx
+++ src/pages/AltAdmin/[usrId].tsx
@@ -133,7 +133,7 @@ const AltAdmin = () => {
                         </div>
                         <div className='mb-4'>
                           <input
-                            type='text'
+                            type='date'
                             className='form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none'
                             placeholder='Informe Nascimento'
                             name='nascimento'
