db870068d12e0157d0c1c81b25a7f8c6fb2bc862
Gilson Fábio <gilsonfabio@gmail.com>
2023-02-28 13:34:42 -0300

    Alteração do versionamento

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -27,15 +27,17 @@ const Menubar = () => {
                     </div>                    
                 </div> 
                 <div className="flex flex-row items-center mr-2 md:mr-36" >
-                    <Link href={'/SignUpForm'}> 
-                        <a className='inline-block px-6 py-2 border-2 border-green-600 text-green-600 hover:border-white hover:text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-green-600 hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'>
-                            Login Admin
-                        </a>  
-                    </Link>            
-                </div>
-                <div className=''>
-                    <span className='text-sm text-green-400'>Versão:1.02 - 28-02-2023</span>
-                </div>                          
+                    <div className=''>
+                        <Link href={'/SignUpForm'}> 
+                            <a className='inline-block px-6 py-2 border-2 border-green-600 text-green-600 hover:border-white hover:text-white font-medium text-xs leading-tight uppercase rounded-full hover:bg-green-600 hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out'>
+                                Login Admin
+                            </a>  
+                        </Link>            
+                    </div>
+                    <div className=''>
+                        <span className='text-xs text-green-400'>Versão:1.02 - 28-02-2023</span>
+                    </div>
+                </div>                              
             </div>
         </nav> 
     )
