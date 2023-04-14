1a6d5ada60a23232c2d7743a532dc404d1545eff
Gilson Fábio <gilsonfabio@gmail.com>
2023-04-14 11:02:45 -0300

    baseUrl atualizada

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -5,7 +5,7 @@ export function getAPIClient(ctx?: any) {
   const { 'nextauth.token': token } = parseCookies(ctx)
 
   const api = axios.create({
-    baseURL: 'https://backsports.herokuapp.com/'
+    baseURL: 'https://backsports.herokuapp.com'
   })
 
   api.interceptors.request.use(config => {
