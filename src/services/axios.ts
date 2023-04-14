3000b9d55f30796f986b91aca97a05d11c62606d
Gilson Fábio <gilsonfabio@gmail.com>
2023-04-14 13:28:37 -0300

    Alteração na chamada da Dashboard II

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -6,7 +6,7 @@ export function getAPIClient(ctx?: any) {
 
   //baseURL: 'https://backsports.herokuapp.com'
   const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://backsports.herokuapp.com'
   })
 
   api.interceptors.request.use(config => {
